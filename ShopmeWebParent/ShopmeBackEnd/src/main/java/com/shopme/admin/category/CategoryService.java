eca2c3e8b0625c76727a360d0e34c379a9b3d514
hqyuh <hoquanghuy0123@gmail.com>
2022-07-08 23:33:13 +0700

    [Add] category form and show select Parent Category

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
@@ -4,7 +4,9 @@ import com.shopme.common.entity.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class CategoryService {
@@ -19,5 +21,38 @@ public class CategoryService {
     public List<Category> listAll() {
         return (List<Category>) categoryRepo.findAll();
     }
+
+    public List<Category> listCategoriesUsedInForm() {
+        List<Category> categoriesUsedInForm = new ArrayList<>();
+
+        Iterable<Category> categoriesInDB = categoryRepo.findAll();
+        for (Category rootCategory : categoriesInDB) {
+            if (rootCategory.getParent() == null) {
+                // children
+                Set<Category> parent = rootCategory.getChildren();
+                for (Category subCategory : parent) {
+                   String name = "--" + subCategory.getName();
+                   categoriesUsedInForm.add(new Category(name));
+                   listChildHierarchicalCategories(categoriesUsedInForm, subCategory, 1);
+                }
+            }
+        }
+
+        return categoriesUsedInForm;
+    }
+
+    private void listChildHierarchicalCategories(List<Category> categoriesUsedInForm,
+                                                 Category parent,
+                                                 int subLevel) {
+        int newSubLevel = subLevel + 1;
+        Set<Category> child = parent.getChildren();
+        for (Category subCategory : child) {
+            String name = "--".repeat(Math.max(0, newSubLevel)) + subCategory.getName();
+            categoriesUsedInForm.add(new Category(name));
+            listChildHierarchicalCategories(categoriesUsedInForm, subCategory, newSubLevel);
+        }
+    }
+
+
 
 }
